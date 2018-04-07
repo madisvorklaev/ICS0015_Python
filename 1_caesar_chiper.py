3268787305ea0bbf83ae7a4f7396083dae3ca2d2
Madis Võrklaev <madisvorklaev@gmail.com>
2018-04-07 08:38:43 +0300

    typo

--- 1_caesar_chiper.py
+++ 1_caesar_chiper.py
@@ -1,7 +1,7 @@
 #Caesar Chiper v1.0:
 #Takes an input string from a text file and en(de)crypts
 #it by shifting the alphabet by a given step. All UPPERCASE characters are
-#converted to lowercase and the shift character is affected.
+#converted to lowercase and the shift character is not affected.
 #The input text must be on one line and consist of basic Latin alphabet characters
 #(abcdefghijklmnopqrstuvwxyz).
 #Tested on Python v.3.6.5
