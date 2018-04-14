a9ea8034678c832525be59feb03bc1d04432be1c
Madis Võrklaev <madisvorklaev@gmail.com>
2018-04-14 22:47:25 +0300

    added print method

--- 2_class_example.py
+++ 2_class_example.py
@@ -1,7 +1,14 @@
+#Class Example v1.0:
+#Uses class to create an object.
+#However, it's allowed to create oen object only.
+
 import time
 
-class Thumbelina:
-        def __init__(self):
+class Fairytale:
+        def __init__(self, character):
+                self.character = character
+
+        def printCharacter(self):
                 check = 1/thumbelinas_left
                 print("    O    ")
                 time.sleep(0.3)
@@ -13,17 +20,15 @@ class Thumbelina:
                 time.sleep(0.3)
                 print("   I I   ")
                 time.sleep(0.3)
-                print("Thumbelina is ready")
+                print(self.character, "is ready")
 
 thumbelinas_left = 1
 while True:
         try:
                 p = input("Enter any character to make a fairytale character: ")
-                Thumbelina()
+                Thumbelina = Fairytale(character = "Thumbelina")
+                Thumbelina.printCharacter()
                 thumbelinas_left -=1
         except ZeroDivisionError as error :
                 print("I'm sorry, Thumbelina is one and only")
                 break
-
-# print(isinstance(blu, Parrot))
-# print(issubclass(Parrot, Bird))
