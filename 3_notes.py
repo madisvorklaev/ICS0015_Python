b7c1c7e4bc708d31df84dd06e082620d35296a15
Madis Võrklaev <madisvorklaev@gmail.com>
2018-04-21 13:08:42 +0300

    basic functionality ok

--- 3_notes.py
+++ 3_notes.py
@@ -1,5 +1,5 @@
 import tkinter
-from tkinter import ttk, scrolledtext, filedialog
+from tkinter import ttk, scrolledtext, filedialog, messagebox
 from tkinter import *
 
 window = Tk()
@@ -22,12 +22,12 @@ scr = scrolledtext.ScrolledText(window, width = scrollW, height = scrollH, wrap 
 
 #Call back function for menus
 def _new():
-	window.quit()
-	window.destroy()
-	exit()
+    contents = ''
+    scr.insert(tkinter.INSERT, contents)
+    scr.grid(column = 0, columnspan = 3)
 
 def _open():
-    filename =  filedialog.askopenfilename(initialdir = 'C:\ ', title = 'Select file', filetypes = (('text files','*.txt'),('all files','*.*')))
+    filename =  filedialog.askopenfilename(initialdir = 'C:\ ', title = 'Select file', filetypes = (('Text Documents','*.txt'),('All Files','*.*')))
     f = open(filename)
     text = f.read()
     f.close()
@@ -35,21 +35,28 @@ def _open():
     scr.insert(tkinter.INSERT, text)
     scr.grid(column = 0, columnspan = 3)
         
-def _save():
-    window.filename =  filedialog.asksaveasfilename(initialdir = 'C:\ ',title = 'Select file',filetypes = (('jpeg files','*.jpg'),('all files','*.*')))
-    print (window.filename)
+##def _save():
+##   # https://python-forum.io/Thread-save-acts-like-save-as
+##    filename =  filedialog.asksaveasfilename(initialdir = 'C:\ ',title = 'Select file',filetypes = (('Text Documents','*.txt'),('All Files','*.*')))
+##    f = open(filename, 'w')
+##    contents = scr.get(1.0, END)
+##    f.write(contents)
+##    f.close
 
 def _saveas():
-    filename =  filedialog.asksaveasfilename(initialdir = 'C:\ ',title = 'Select file',filetypes = (('text files','*.txt'),('all files','*.*')))
+    filename =  filedialog.asksaveasfilename(initialdir = 'C:\ ',title = 'Select file',filetypes = (('Text Documents','*.txt'),('All Files','*.*')))
     f = open(filename, 'w')
     contents = scr.get(1.0, END)
     f.write(contents)
     f.close
 
 def _quit():
-	window.quit()
-	window.destroy()
-	exit()
+    window.quit()
+    window.destroy()
+    exit()
+
+def _about():
+    messagebox.showinfo("Notes v1.0", "Notes by Madis Võrklaev")
 
 #creating Menu Bar
 menuBar = tkinter.Menu(window) 
@@ -57,15 +64,14 @@ window.config(menu = menuBar)
 fileMenu = tkinter.Menu(menuBar, tearoff = 0)
 fileMenu.add_command(label = 'New', command = _new)
 fileMenu.add_command(label = 'Open', command = _open)
-fileMenu.add_command(label = 'Save', command = _save)
+##fileMenu.add_command(label = 'Save', command = _save)
 fileMenu.add_command(label = 'Save as', command = _saveas)
 menuBar.add_cascade(label = 'File', menu = fileMenu)
 fileMenu.add_separator()
 fileMenu.add_command(label = 'Exit', command = _quit) 
 
 helpMenu = tkinter.Menu(menuBar, tearoff = 0)
-helpMenu.add_command(label = '?')
-helpMenu.add_command(label ='About')
+helpMenu.add_command(label ='About', command = _about)
 menuBar.add_cascade(label = 'Help', menu = helpMenu)
 
 # Starting main window (root window)
