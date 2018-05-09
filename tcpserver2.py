49b523336ce7e122f791ccf541ea8f1968f25707
Madis <madisvorklaev@gmail.com>
2018-05-09 22:52:36 +0300

    server supports multiple clients and threads, howCode@YouTube

--- tcpserver2.py
+++ tcpserver2.py
@@ -1,26 +1,29 @@
 import socket
 import threading
 
-def Main():
-	host = '127.0.0.1'
-	port = 5000
-
 class Server():
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.bind((host,port))
-	print('Server Running.....')
-	s.listen(1)
-	client, address = s.accept()
-	print('Connection from: ', str(address))
-	while True:
-		data = client.recv(1024).decode('utf-8')
-		if not data:
-			break
-		print('from connected user: ' + data)
-		data = data.upper()
-		print('sending: ' + data)
-		client.send(data.encode('utf-8'))
-	client.close()
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    connections = []
+    def __init__(self):
+        self.s.bind(('127.0.0.1', 5000))
+        print('Server Running.....')
+        self.s.listen(1)
+        
+    def handler(self, client, address):
+        while True:
+            data = client.recv(1024).decode('utf-8')
+            for connection in connections:
+                connection.send(data)
+            if not data:
+                break
 
-if __name__ == '__main__':
-    Main()
+    def run(self):
+        client, address = self.s.accept()
+        cThread = threading.Thread(target=self.handler, args=(client, address))
+        cThread.daemon = True
+        cThread.start()
+        self.connections.append(client)
+        print(self.connections)
+
+server = Server()
+server.run()
