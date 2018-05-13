91fc5a60b9645d9c6f9294a505da34f70e04c49d
Madis <madisvorklaev@gmail.com>
2018-05-13 23:13:58 +0300

    Ugly, but works

--- chat.py
+++ chat.py
@@ -33,32 +33,10 @@ class Server:
             print(self.connections)
 #            if (len(self.connections)) > 5:
 #                c.send(b'Sorry, too many connections')
+#                c.send(b'{quit}')
 #                self.connections.remove(c)
 #                c.close()
 
-#class Client:
-#    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-#    def sendMsg(self):
-#        while True:
-#            self.s.send(bytes(input(""), 'utf-8'))
-
-#    def __init__(self, a):
-#        self.s.connect((a, 5000))
-#        iThread = threading.Thread(target=self.sendMsg)
-#        iThread.daemon = True
-#        iThread.start()
-
-#        while True:
-#            data = self.s.recv(1024)
-#            if not data:
-#                break
-#            print(str(data, 'utf-8'))
-#            
-
-#if (len(sys.argv) > 1):
-#    client = Client(sys.argv[1])
-#else:
 server = Server()
 server.run()
 
--- chatclient.py
+++ chatclient.py
@@ -1,26 +1,51 @@
 # The network part is from https://www.youtube.com/watch?v=D0SLpD7JvZI
+# https://medium.com/swlh/lets-write-a-chat-app-in-python-f6783a9ac170
 
 import socket
 import threading
 import sys
+import tkinter
+from tkinter import ttk, scrolledtext, filedialog, messagebox
+from tkinter import *
 
-class Client:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def send_message():
+    m = message_field.get()
+    message_field.delete(0, END)
+    s.send(bytes(m+ '\n', 'utf8'))
+    if m == '/quit':
+        s.close()
+        window.quit()
 
-    def sendMsg(self):
-        while True:
-            self.s.send(bytes(input(""), 'utf-8'))
+def receive_message():
+    while True:
+        data = s.recv(1024)
+        if not data:
+            break
+        print(str(data, 'utf-8'))
+        message_show.insert(tkinter.END, data)
+        message_show.see(END)
 
-    def __init__(self, a):
-        self.s.connect((a, 5000))
-        iThread = threading.Thread(target=self.sendMsg)
-        iThread.daemon = True
-        iThread.start()
+window = Tk()
+window.title('Chat')
+window.geometry('960x540')
 
-        while True:
-            data = self.s.recv(1024)
-            if not data:
-                break
-            print(str(data, 'utf-8'))
+frame = tkinter.Frame()
+message_show = scrolledtext.ScrolledText(window, wrap = tkinter.WORD)
+frame.pack()
+message_show.pack(side = LEFT, fill = BOTH, expand = YES)
+message_field = tkinter.Entry(frame)
+message_field.pack()
+send_button = tkinter.Button(frame, text='SEND', command = send_message)
+send_button.pack()
+addr = ('127.0.0.1', 5000)
 
-client = Client(sys.argv[1])
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(addr)
+
+receive_thread = threading.Thread(target=receive_message)
+receive_thread.start()
+
+tkinter.mainloop()
+
+
+
